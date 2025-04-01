be4f9e356e8556acf79a79503b6c8cbf826d4424
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-04-01 08:43:09 -0300

    Ejercicio 1 terminado

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -12,7 +12,14 @@ public class DeclaracionDeVariables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        cantidadAlumnos = 32;
+        kilosVendidos = 23.7f;
+        nombreAlumno = "Jerónimo";
+        estaAprobado = false;
+        Debug.Log(cantidadAlumnos);
+        Debug.Log(kilosVendidos);
+        Debug.Log(nombreAlumno);
+        Debug.Log(estaAprobado);
     }
 
     // Update is called once per frame
