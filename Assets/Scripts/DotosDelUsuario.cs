2376fe40d42db26b56ae52afd951a6b6bfbe8870
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-04-01 09:10:28 -0300

    Datos del usuario empezado

---
+++ Assets/Scripts/DotosDelUsuario.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotosDelUsuario : MonoBehaviour
+{
+    public string nombreUsuario;
+    public int edadUsuario;
+    public float alturaUsuario;
+    public bool esDonante;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombreUsuario);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
