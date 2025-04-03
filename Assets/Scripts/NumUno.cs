b6245216fc006b5c28454d4c895bf27a14b1ffa1
Federico Zonis <49090960@est.ort.edu.ar>
2025-04-03 12:07:00 -0300

    NumUno terminado

---
+++ Assets/Scripts/NumUno.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumUno : MonoBehaviour
+{
+    int num1=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        num1 += 2;
+        Debug.Log(num1);
+        num1 *= num1;
+        Debug.Log(num1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
